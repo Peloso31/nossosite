8a353f6bf3f9805c1bb2f5fa9790781a473683c4
Peloso31 <pelloso93@gmail.com>
2023-02-28 14:49:52 -0300

    update css, js e html
    html > feedback e-mail
    js > pop-up de obs
    css > exclusão de itens desnecessários

--- Js/script.js
+++ Js/script.js
@@ -14,5 +14,16 @@ function Login() {
   
   document.getElementById("btnEntrar").addEventListener("click", Login);
 
-
+  if(screen.width >= 768) {
+    var div = document.createElement('div');
+    div.innerHTML = '<p>Este site foi projetado para dispositivos móveis. Para uma melhor experiência, utilize em um dispositivo móvel.</p>';
+    div.style.backgroundColor = '#F9F9F9';
+    div.style.padding = '10px';
+    div.style.textAlign = 'center';
+    div.style.position = 'fixed';
+    div.style.bottom = '0';
+    div.style.left = '0';
+    div.style.width = '100%';
+    document.body.appendChild(div);
+  }
   
